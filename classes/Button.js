e4cf3b2aded2809cddf7cd080ff226121e9a1c73
slawek00 <slawekjalojda@gmail.com>
2023-02-21 19:18:23 +0100

    add static menu

--- classes/Button.js
+++ classes/Button.js
@@ -1,17 +1,21 @@
 export {Button as default}
 
 class Button{
-    constructor(strURL) {
+    constructor(strURL, {x, y}, {width, height}) {
         this.position = {
-            x:0,
-            y:0
+            x:x,
+            y:y
         }
         this.size = {
-            width:0,
-            height:0
+            width:width,
+            height:height
         }
 
         this.image = new Image()
         this.image.src = strURL
+    }
+
+    draw(context){
+        context.drawImage(this.image,this.position.x,this.position.y,this.size.width, this.size.height)
     }
 }
--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-'use strict'
 addEventListener('load', main)
+
+import Button from "./classes/Button.js";
 
 function main(){
     const canvas = document.querySelector("canvas"),
         context = canvas.getContext("2d"),
         background = new Image()
 
+
+
     background.src = "./assets/GUI/menu/bg.png"
-    context.drawImage(background,0,0,300,150)
+
 }
