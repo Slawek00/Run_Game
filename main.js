6d183a108f5c9e65787bea09466351149bbf29c1
slawek00 <slawekjalojda@gmail.com>
2023-02-26 18:21:01 +0100

    Zmiana koncepcji projektu, dodanie przycisków do menu

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
 addEventListener('load', main)
-
-import Button from "./classes/Button.js";
-
 function main(){
-    const canvas = document.querySelector("canvas"),
-        context = canvas.getContext("2d"),
-        background = new Image()
 
 
-
-    background.src = "./assets/GUI/menu/bg.png"
 
 }
